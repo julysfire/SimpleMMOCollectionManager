9161f003cb0002bdf88b091768330be6143befc6
julysfire <blackflame0729@yahoo.com>
2021-08-06 23:31:52 -0500

    1.0 Finished.

--- src/background.js
+++ src/background.js
@@ -1,26 +1,48 @@
-//Background workings
+//Background working
+var currentTabId;
 
+
+//
 //First time install, setup storage
+//
 chrome.runtime.onInstalled.addListener(function(details){
 	if(details.reason == "install"){
 		chrome.storage.local.set({items: ""}, function(){
 		});
+		chrome.storage.local.set({newItem: ""}, function(){
+		});
 	}
 });
 
+
+//
 //Tab Listeners
+//
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	if(changeInfo.status == "complete"){
+		currentTabId = tabId;
 		if(tab.url.indexOf("collection/collectables") != -1 || tab.url.indexOf("collection/items") != -1){
-			chrome.tabs.sendMessage(tabId, {text: 'report_back'}, storeCollectionItems);
+			chrome.tabs.sendMessage(currentTabId, {text: 'report_back'}, storeCollectionItems);
 		}else if(tab.url.indexOf("inventory/items") != -1){
-			chrome.tabs.sendMessage(tabId, {text: 'report_back'}, checkInventory);
+			chrome.tabs.sendMessage(currentTabId, {text: 'report_back'}, checkInventory);
 		}
 	}
 });
 
+
 //
-// Mainly storage update method
+//Message listener from content_scripts
+//
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.text === 'new_item'){
+        saveNewItem(msg.data);
+        sendResponse();
+    }
+});
+
+
+//
+// Main storage update method
 //
 function storeCollectionItems(str){
 	//Get all item names
@@ -39,25 +61,82 @@ function storeCollectionItems(str){
 				dataString = dataString+";"+items[i];
 			}
 		}
-		console.log(dataString);
 		chrome.storage.local.set({items: dataString}, function(){
 		});
 	});
 }
 
+
 //
-// Mainly inventory checking method
+// Main inventory checking method
 //
 function checkInventory(str){
 	var invItems = [];
+
+	//Check if a new item was just added
+	if(str.search('class="rounded-md bg-green-50 p-4 my-4"') > -1){
+		newItemAdded();
+	}
+
 	while(str.search('id="item-id') > -1){
 		str = str.substring(str.search('id="item-id'),str.length);
+
 		//Don't worry about foods, i.e. uncollectable
 		var subStr = str.substring(str.search('<button type="button"'),str.length);
 		subStr = subStr.substring(subStr.search(">")+2,subStr.search("</b")-1);
+
 		//Push to array
-		if(subStr == "Collect" || subStr == "Equip") invItems.push(str.substring(str.search(">")+1,str.search("<")));
+		if(subStr == "Collect" || subStr == "Equip") invItems.push([str.substring(str.search(">")+1,str.search("<")), str.substring(12,str.search(">")-1),""]);
+		else invItems.push(["zzz","zzz","zzz"]);
 		str = str.substring(1,str.length);
 	}
-	console.log(invItems);
+	var dataString = "";
+
+	//Check if items are already collected or not
+	chrome.storage.local.get(["items"], function(data){
+		dataString = data.items+"";
+		for(var i =0;i<invItems.length;i++){
+			if(dataString.search(invItems[i][0]) > 0){
+				invItems[i][2] = "collected"
+			}else if(invItems[i][0] == "zzz"){
+				invItems[i][2] = "zzz"
+			}else invItems[i][2] = "need"
+		}
+		//Send list over to content
+		chrome.tabs.sendMessage(currentTabId, {text: "inject_icons", data: invItems}, noCallback);
+	});
+}
+
+
+//
+// For copying the new item into the local storage in case it is added to collection
+//
+function saveNewItem(data){
+	chrome.storage.local.set({newItem: data}, function(){
+	});
+}
+
+
+//
+//For checking/adding new items to the items storage
+//
+function newItemAdded(){
+	chrome.storage.local.get(["newItem","items"],function(data){
+		var nItem = data.newItem+"";
+		var itemList = data.items+"";
+
+		if(itemList.search(nItem) == -1){
+			itemList = itemList+";"+nItem;
+			chrome.storage.local.set({items: itemList}, function(){
+			});
+		}
+	});
+
+	//Reset newItem storage
+	chrome.storage.local.set({newItem: ""}, function(){
+	});
+}
+
+function noCallback(){
+	//
 }
--- src/content.js
+++ src/content.js
@@ -3,16 +3,35 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.text === 'report_back') {
         sendResponse(document.all[0].outerHTML);
     }
+    if (msg.text === 'inject_icons'){
+        injectIcons(msg.data);
+        sendResponse();
+    }
 });
 
-function injectIcons(){
-  var divs = document.getElementsByTagName("div");
-  for(var i =0;i<divs.length;i++){
-    if(divs[i].className == "flex items-center"){
-      divs[i].id = "itemIcon"+[i];
-
-      var nDiv = document.createElement("div");
-      var img = document.createElement("img");
-    }
+function injectIcons(inventoryItems){
+  for(var i = 0;i<inventoryItems.length;i++){
+    var spanElement = document.getElementById("item-id-"+inventoryItems[i][1]);
+    var img = new Image();
+    if(inventoryItems[i][2] == "need") img.src = chrome.runtime.getURL("newItem.png");
+    else img.src = chrome.runtime.getURL("checkmark.png");
+    if(spanElement != null) spanElement.appendChild(img);
   }
+}
+
+//Add new item to collection Listener
+document.addEventListener('DOMNodeInserted', newNode);
+
+//CHeck for the item and check to see if it was added
+function newNode(data){
+  if(data.relatedNode.type == "button" && data.relatedNode.className == "swal2-confirm"){
+    var itemName = document.getElementById("swal2-title").textContent;
+    chrome.runtime.sendMessage({text: "new_item", data: itemName},function(response){
+      //nah
+    });
+  }
+}
+
+function noCallback(){
+	//
 }
